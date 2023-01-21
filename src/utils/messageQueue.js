9fc144f8fa0c87767c0e405291b96a3f19d05038
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2023-01-21 12:50:51 +0530

    added subscribe message function

--- src/utils/messageQueue.js
+++ src/utils/messageQueue.js
@@ -12,6 +12,23 @@ const createChannel = async () => {
     }
 }
 
+const subscribeMessage = async (channel, service,  binding_key) => {
+    try {
+        const applicationQueue = await channel.assertQueue('QUEUE_NAME');
+
+        channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binding_key);
+
+        channel.consume(applicationQueue.queue, msg => {
+            console.log('received data');
+            console.log(msg.content.toString());
+            channel.ack(msg);
+        });
+    } catch (error) {
+        throw error;
+    }
+    
+}
+
 const publishMessage = async (channel, binding_key, message) => {
     try {
         await channel.assertQueue('QUEUE_NAME');
